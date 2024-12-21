929bc69b16277fee9827f1d2f1692ecf3924b7d7
Rafal <rafek131@wp.pl>
2024-12-21 13:34:32 +0100

    'vite.config' - added 'base' rule to fix path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
+const baseUrl = process.env.NODE_ENV === 'development' ? './' : '/where2eat-vue/';
+
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [vue(), vueDevTools()],
@@ -12,6 +14,7 @@ export default defineConfig({
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
+	base: baseUrl,
 	test: {
 		globals: true,
 		environment: 'happy-dom',
